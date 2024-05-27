2426fc662d86bc65faacb4527d72168f87b1b3ea
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-05-27 22:50:59 +0200

    Add: Bonus 1 (button like)

--- js/script.js
+++ js/script.js
@@ -4,13 +4,34 @@ createApp({
   data() {
     return {
       discs: [],
+      apiUrl: "http://localhost/boolean/php-dischi-json/server.php",
     };
   },
   created() {
     axios
       .get("http://localhost/boolean/php-dischi-json/server.php")
       .then((resp) => {
-        this.discs = resp.data.results;
+        this.discs = resp.data;
+        console.log(resp);
+        console.log(this.discs);
       });
+  },
+
+  methods: {
+    likedDisco(index) {
+      console.log(index);
+      this.discs[index].disco_liked = !this.discs[index].disco_liked;
+      const data = {
+        index: index,
+      };
+      axios
+        .post(this.apiUrl, data, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((resp) => {
+          console.log(resp);
+          //this.discs = resp.data; non salvarla o rimane il cuore
+        });
+    },
   },
 }).mount("#app");
--- server.php
+++ server.php
@@ -7,7 +7,17 @@ $dischi_array = json_decode($dischi_json, true); //array json
 
 
 //LOGICA
+//BONUS 1
+for ($i = 0; $i < count($dischi_array); $i++) {
+    $dischi_array[$i]['disco_liked'] = false;
+};
 
+if (isset($_POST["index"])) {
+    $dischi_array[$_POST["index"]]["disco_liked"] = !$dischi_array[$_POST["index"]]["disco_liked"];
+};
+
+file_put_contents("dischi.json", json_encode($dischi_array));
+//____________________________________________________
 
 //Output: strutturo la risposta API in un ARRAY ASSOCIATIVO
 $response = [
@@ -16,6 +26,8 @@ $response = [
 ]; //Array associativo che in Postman lo vedi come array di object
 
 
-$response_json = json_encode($response);//string
+$response_json = json_encode($response); //string
+//serve ad impostare la comunicazione http
 header("Content-type: application/json");
 echo $dischi_json;
+// echo $response_json;
